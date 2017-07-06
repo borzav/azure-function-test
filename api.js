dedc09a84e769049f3745e3b4719d42c3ee1256c
Borza Viktor <viktor.borza@jaydata.org>
2017-07-06 13:09:54 +0200

    change

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 module.exports = {
     api: function (context, req) {
-        context.log('api call test')
+        context.log('api call test 1')
 
         if (req.query.name || (req.body && req.body.name)) {
             context.res = {
@@ -18,7 +18,25 @@ module.exports = {
     },
     
     hello: function (context, req) {
-        context.log('api call test')
+        context.log('api call test 2')
+
+        if (req.query.name || (req.body && req.body.name)) {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: "Hello " + (req.query.name || req.body.name)
+            };
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: "Please pass a name on the query string or in the request body"
+            };
+        }
+        context.done();
+    },
+    
+    hello2: function (context, req) {
+        context.log('api call test 3')
 
         if (req.query.name || (req.body && req.body.name)) {
             context.res = {
